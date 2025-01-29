0b1285a8a06e761dc494cc3a360a6ac2a97ae8c6
Suikyo <judeanthony02sayson@gmail.com>
2025-01-30 06:47:47 +0800

    server-side in progress

---
+++ host/index.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const app = express();
+
+let accounts = [
+    "Suikyoo": 
+]
+
+let data = [
+    {
+        "user": "Suikyoo",
+        "message": "the quick brown fox jumped over the lazy dog",
+        "id": 0
+    },
+    {
+        "user": "Delta",
+        "message": "lorem ipsum dolor amet",
+        "id": 1
+    },
+    {
+        "user": "Gamma",
+        "message": "wubalubadubdub",
+        "id": 2
+    }
+
+];
+
