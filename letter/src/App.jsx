9d6938a335d1e84b61e8137266f90ba43bfaf834
Suikyo <judeanthony02sayson@gmail.com>
2025-02-13 08:04:13 +0800

    fds

--- letter/src/App.jsx
+++ letter/src/App.jsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
+import reactLogo from '/assets/react.svg';
 import viteLogo from '/vite.svg';
-import ConvoTab from './components/ConvoTab';
-import { AuthProvider } from './auth';
+import ConvoTab from '/components/ConvoTab';
+import { AuthProvider } from '/auth';
 
-import Home from './pages/Home';
-import Login from './pages/Login';
-import MainPage from './pages/MainPage';
-import MessageApp from './pages/MessageApp';
-import ProtectedRoute from './components/ProtectedRoute';
+import Home from '/pages/Home';
+import Login from '/pages/Login';
+import MainPage from '/pages/MainPage';
+import MessageApp from '/pages/MessageApp';
+import ProtectedRoute from '/components/ProtectedRoute';
 
-import "./styles/style.css";
+import "/styles/style.css";
 
 const App = () => {
     
